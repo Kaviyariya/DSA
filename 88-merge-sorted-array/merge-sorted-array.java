61f3a7f72ed8ccc283387a5502a24d3aaefd544c
Kaviya balamurugan <145695544+Kaviyariya@users.noreply.github.com>
2026-05-07 21:36:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (75.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -15,11 +15,6 @@ class Solution {
             }
             k--;
         }
-        while(i>0){
-            nums1[k]=nums1[i];
-            i--;
-            k--;
-        }
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
