d372012c83801658e46c0b5b287192b2b96e155e
Kaviya balamurugan <145695544+Kaviyariya@users.noreply.github.com>
2026-06-16 09:57:34 +0530

    Time: 4 ms (76.47%) | Memory: 55.3 MB (58.82%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '%') {
+                sb.reverse();
+            } else if (ch == '*') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else if (ch == '#') {
+                sb.append(sb); 
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
