8d6469e1107e9718944d89e3e98a1e297e1ee971
Kaviya balamurugan <145695544+Kaviyariya@users.noreply.github.com>
2026-06-22 09:40:11 +0530

    Time: 7 ms (36.15%) | Memory: 45.4 MB (45.64%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,31 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer>st=new Stack<>();
+        int n1=0;
+        int n2=0;
         int res=0;
         for(int i=0;i<tokens.length;i++){
             if(tokens[i].equals("-")){
-               int n2=(!st.isEmpty()?st.pop():0);
-                int n1=(!st.isEmpty()?st.pop():0);
+               n2=(!st.isEmpty()?st.pop():0);
+               n1=(!st.isEmpty()?st.pop():0);
                 res=n1-n2;
                 st.push(res);
             }else if(tokens[i].equals("/")){
-                int n2=(!st.isEmpty()?st.pop():0);
-                int n1=(!st.isEmpty()?st.pop():0);
+                n2=(!st.isEmpty()?st.pop():0);
+                n1=(!st.isEmpty()?st.pop():0);
                 res=n1/n2;
                 st.push(res);
             }else if(tokens[i].equals("*")){
-                int n2=(!st.isEmpty()?st.pop():0);
-                int n1=(!st.isEmpty()?st.pop():0);
+                n2=(!st.isEmpty()?st.pop():0);
+                 n1=(!st.isEmpty()?st.pop():0);
                 res=n1*n2;
                 st.push(res);
             }else if(tokens[i].equals("+")){
-                int n2=(!st.isEmpty()?st.pop():0);
-                int n1=(!st.isEmpty()?st.pop():0);
+                n2=(!st.isEmpty()?st.pop():0);
+                n1=(!st.isEmpty()?st.pop():0);
                 res=n1+n2;
                 st.push(res);
             }else{
-                int n=Integer.parseInt(tokens[i]);
-                st.push(n);
+                st.push(Integer.parseInt(tokens[i]));
             }
         }
         return st.pop();
