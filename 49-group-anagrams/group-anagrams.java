81e541306b8ccb437150410a24d75c6362a1ac90
Kaviya balamurugan <145695544+Kaviyariya@users.noreply.github.com>
2026-06-29 17:32:52 +0530

    Time: 7 ms (73.14%) | Memory: 50.1 MB (22.24%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,20 @@
 class Solution {
-    static boolean anagram(String s1,String s2){
-        if(s1.length()!=s2.length()) return false;
-        char c1[]=s1.toCharArray();
-        char c2[]=s2.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        s1=new String(c1);
-        s2=new String(c2);
-        return s1.equals(s2);
-    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n=strs.length;
-        Arrays.sort(strs);
-        List<List<String>>ans=new ArrayList<>();
-        int vis[]=new int[n];
-        for(int i=0;i<n;i++){
-            if(vis[i]==-1) continue;
-            vis[i]=-1;
-           List<String>list=new ArrayList<>();
-           list.add(strs[i]);
-           for(int j=i+1;j<n;j++){
-            if(vis[j]==-1) continue;
-            if(anagram(strs[i],strs[j])){
-                vis[j]=-1;
-                list.add(strs[j]);
+        List<List<String>>list=new ArrayList<>();
+        Map<String,List<String>>map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String word=strs[i];
+            char ch[]=word.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
             }
-           }
-           ans.add(list);
+            map.get(key).add(word);
         }
-        return ans;
+        for(String l:map.keySet()){
+            list.add(map.get(l));
+        }
+        return list;
     }
 }
